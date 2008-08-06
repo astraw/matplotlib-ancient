astraw/matplotlib-ancient

commit 2d429127358b92416adbffb4947a7796f17f3846

Fix RGBA arrays in collections.
git-svn-id: file:///mnt/workdisk/tmp/matplotlib-svn-rsync/trunk/matplotlib@5976 f61c4167-ca0d-0410-bb4a-bb21726e55ed

lib/matplotlib/axes.py
@@ -3797,14 +3797,18 @@ class Axes(martist.Artist):
         # if color looks like a color string, an RGB tuple or a
         # scalar, then repeat it by nbars
         if (is_string_like(color) or
-            (iterable(color) and len(color)==3 and nbars!=3) or
+            (iterable(color) and
+             len(color) in (3, 4) and
+             nbars != len(color)) or
             not iterable(color)):
             color = [color]*nbars
 
         # if edgecolor looks like a color string, an RGB tuple or a
         # scalar, then repeat it by nbars
         if (is_string_like(edgecolor) or
-            (iterable(edgecolor) and len(edgecolor)==3 and nbars!=3) or
+            (iterable(edgecolor) and
+             len(edgecolor) in (3, 4) and
+             nbars != len(edgecolor)) or
             not iterable(edgecolor)):
             edgecolor = [edgecolor]*nbars
 

lib/matplotlib/collections.py
@@ -332,6 +332,7 @@ class Collection(artist.Artist, cm.ScalarMappable):
         """
         if c is None: c = mpl.rcParams['patch.facecolor']
         self._facecolors = _colors.colorConverter.to_rgba_array(c, self._alpha)
+        self._facecolors_original = c
 
     set_facecolors = set_facecolor
 
@@ -363,6 +364,7 @@ class Collection(artist.Artist, cm.ScalarMappable):
         else:
             if c is None: c = mpl.rcParams['patch.edgecolor']
             self._edgecolors = _colors.colorConverter.to_rgba_array(c, self._alpha)
+            self._edgecolors_original = c
 
     set_edgecolors = set_edgecolor
 
@@ -378,11 +380,13 @@ class Collection(artist.Artist, cm.ScalarMappable):
         else:
             artist.Artist.set_alpha(self, alpha)
             try:
-                self._facecolors[:, 3] = alpha
+                self._facecolors = _colors.colorConverter.to_rgba_array(
+                    self._facecolors_original, self._alpha)
             except (AttributeError, TypeError, IndexError):
                 pass
             try:
-                self._edgecolors[:, 3] = alpha
+                self._edgecolors = _colors.colorConverter.to_rgba_array(
+                    self._edgecolors_original, self._alpha)
             except (AttributeError, TypeError, IndexError):
                 pass
 

lib/matplotlib/patches.py
@@ -263,7 +263,6 @@ class Patch(artist.Artist):
             gc.set_linewidth(self._linewidth)
             gc.set_linestyle(self._linestyle)
 
-        gc.set_alpha(self._alpha)
         gc.set_antialiased(self._antialiased)
         self._set_gc_clip(gc)
         gc.set_capstyle('projecting')
@@ -271,8 +270,11 @@ class Patch(artist.Artist):
         if (not self.fill or self._facecolor is None or
             (cbook.is_string_like(self._facecolor) and self._facecolor.lower()=='none')):
             rgbFace = None
+            gc.set_alpha(1.0)
         else:
-            rgbFace = colors.colorConverter.to_rgb(self._facecolor)
+            r, g, b, a = colors.colorConverter.to_rgba(self._facecolor, self._alpha)
+            rgbFace = (r, g, b)
+            gc.set_alpha(a)
 
         if self._hatch:
             gc.set_hatch(self._hatch )

commit 2b2ad1fc5a5e400aa494b472a91b96b840c3c3c1

Fix no-fill bug introduced in r5976
git-svn-id: file:///mnt/workdisk/tmp/matplotlib-svn-rsync/trunk/matplotlib@5979 f61c4167-ca0d-0410-bb4a-bb21726e55ed

lib/matplotlib/axes.py
@@ -5195,26 +5195,14 @@ class Axes(martist.Artist):
             ymax = 10**ymax
             self.set_yscale('log')
 
-        class HexagonBinCollection(mcoll.PolyCollection):
-            """A HexagonBinCollection is a PolyCollection where the edge
-               colors are always kept equal to the fill colors"""
-            def update_scalarmappable(self):
-                mcoll.PolyCollection.update_scalarmappable(self)
-                self._edgecolors = self._facecolors
-
         if edgecolors=='none':
-            collection = HexagonBinCollection(
-                        polygons,
-                        linewidths = linewidths,
-                        transOffset = self.transData,
-                        )
-        else:
-            collection = mcoll.PolyCollection(
-                        polygons,
-                        edgecolors = edgecolors,
-                        linewidths = linewidths,
-                        transOffset = self.transData,
-                        )
+            edgecolors = 'face'
+        collection = mcoll.PolyCollection(
+            polygons,
+            edgecolors = edgecolors,
+            linewidths = linewidths,
+            transOffset = self.transData,
+            )
 
         # Transform accum if needed
         if bins=='log':

lib/matplotlib/collections.py
@@ -8,7 +8,7 @@ counterparts (e.g. you may not be able to select all line styles) but
 they are meant to be fast for common use cases (e.g. a bunch of solid
 line segemnts)
 """
-import math, warnings
+import copy, math, warnings
 import numpy as np
 import numpy.ma as ma
 import matplotlib as mpl
@@ -331,8 +331,8 @@ class Collection(artist.Artist, cm.ScalarMappable):
         ACCEPTS: matplotlib color arg or sequence of rgba tuples
         """
         if c is None: c = mpl.rcParams['patch.facecolor']
-        self._facecolors = _colors.colorConverter.to_rgba_array(c, self._alpha)
         self._facecolors_original = c
+        self._facecolors = _colors.colorConverter.to_rgba_array(c, self._alpha)
 
     set_facecolors = set_facecolor
 
@@ -361,10 +361,11 @@ class Collection(artist.Artist, cm.ScalarMappable):
         """
         if c == 'face':
             self._edgecolors = 'face'
+            self._edgecolors_original = 'face'
         else:
             if c is None: c = mpl.rcParams['patch.edgecolor']
-            self._edgecolors = _colors.colorConverter.to_rgba_array(c, self._alpha)
             self._edgecolors_original = c
+            self._edgecolors = _colors.colorConverter.to_rgba_array(c, self._alpha)
 
     set_edgecolors = set_edgecolor
 
@@ -385,8 +386,9 @@ class Collection(artist.Artist, cm.ScalarMappable):
             except (AttributeError, TypeError, IndexError):
                 pass
             try:
-                self._edgecolors = _colors.colorConverter.to_rgba_array(
-                    self._edgecolors_original, self._alpha)
+                if self._edgecolors_original != 'face':
+                    self._edgecolors = _colors.colorConverter.to_rgba_array(
+                        self._edgecolors_original, self._alpha)
             except (AttributeError, TypeError, IndexError):
                 pass
 
@@ -809,7 +811,7 @@ class LineCollection(Collection):
             pickradius=pickradius,
             **kwargs)
 
-        self._facecolors = np.array([])
+        self.set_facecolors([])
         self.set_segments(segments)
 
     def get_paths(self):

lib/matplotlib/colors.py
@@ -332,7 +332,7 @@ class ColorConverter:
         if len(c) == 0:
             return np.zeros((0,4), dtype=np.float_)
         try:
-            result = [self.to_rgba(c, alpha)]
+            result = np.array([self.to_rgba(c, alpha)], dtype=np.float_)
         except ValueError:
             # If c is a list it must be maintained as the same list
             # with modified items so that items can be appended to
@@ -340,18 +340,10 @@ class ColorConverter:
             if isinstance(c, np.ndarray):
                 if len(c.shape) != 2:
                     raise ValueError("Color array must be two-dimensional")
-                if c.shape[1] != 4:
-                    output = np.zeros((c.shape[0], 4))
-                else:
-                    output = c
-            elif not isinstance(c, list):
-                output = list(c)
-            else:
-                output = c
 
+            result = np.zeros((len(c), 4))
             for i, cc in enumerate(c):
-                output[i] = self.to_rgba(cc, alpha)  # change in place
-            result = output
+                result[i] = self.to_rgba(cc, alpha)  # change in place
         return np.asarray(result, np.float_)
 
 colorConverter = ColorConverter()